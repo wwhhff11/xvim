8a830f966cbc24c1006148ae9c3fde34fd7a1784
wanhongfei <wanhongfei@bytedance.com>
2018-06-17 04:03:43 +0800

    fix v4

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
-ln -s ~/xvim/.vimrc ~
-ln -s ~/xvim/.vimrc.bundles ~
-ln -s ~/xvim/.vim ~
+#!/bin/bash
 
-cp ~/xvim/.vim/bundle/Solarized/colors/solarized.vim  ~/.vim/colors
+if test -f ~/.vimrc; then
+	mv ~/.vimrc ~/.vimrc.bak
+fi
+ln -s ~/xvim/.vimrc ~
+
+if test -f ~/.vimrc.bundles;then 
+	mv ~/.vimrc.bundles ~/.vimrc.bundles.bak
+fi
+ln -s ~/xvim/.vimrc.bundles ~
+
+if test -d ~/.vim;then
+	mv ~/.vim ~/.vim.bak
+fi
+ln -s ~/xvim/.vim ~
